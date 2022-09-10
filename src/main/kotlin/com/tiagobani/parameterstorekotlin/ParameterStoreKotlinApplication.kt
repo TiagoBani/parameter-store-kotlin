499019c55a22914adc54a1ec368067a2237dea32
tiagobani <tiago.bani@yahoo.com.br>
2022-09-09 22:03:29 -0300

    add CommandLineRunner

--- src/main/kotlin/com/tiagobani/parameterstorekotlin/ParameterStoreKotlinApplication.kt
+++ src/main/kotlin/com/tiagobani/parameterstorekotlin/ParameterStoreKotlinApplication.kt
@@ -1,10 +1,19 @@
 package com.tiagobani.parameterstorekotlin
 
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class ParameterStoreKotlinApplication
+class ParameterStoreKotlinApplication {
+
+	private val log = LoggerFactory.getLogger(ParameterStoreKotlinApplication::class.java)
+	@Bean
+	fun init() = CommandLineRunner {
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<ParameterStoreKotlinApplication>(*args)
