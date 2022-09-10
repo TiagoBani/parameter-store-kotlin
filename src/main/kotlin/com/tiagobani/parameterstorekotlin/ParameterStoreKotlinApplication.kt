91a9a6fdb33e3288184b3a41b25bedbba04e41f6
tiagobani <tiago.bani@yahoo.com.br>
2022-09-10 00:27:42 -0300

    feat: add parameter store config

--- src/main/kotlin/com/tiagobani/parameterstorekotlin/ParameterStoreKotlinApplication.kt
+++ src/main/kotlin/com/tiagobani/parameterstorekotlin/ParameterStoreKotlinApplication.kt
@@ -1,17 +1,23 @@
 package com.tiagobani.parameterstorekotlin
 
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class ParameterStoreKotlinApplication {
+class ParameterStoreKotlinApplication(
+	@Value("\${param:default}")
+	private val param: String
+) {
 
 	private val log = LoggerFactory.getLogger(ParameterStoreKotlinApplication::class.java)
 	@Bean
 	fun init() = CommandLineRunner {
+		log.info("param => $param")
 	}
 }
 
